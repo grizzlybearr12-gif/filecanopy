8d86d593a61a0282c9996354f0537919c7f381be
Mohit Kochar <grizzlybearr12@gmail.com>
2026-01-06 10:37:59 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6924184329-396b7",
+  "appId": "1:951981757096:web:5d8fc3d93e0b2d685d289d",
+  "apiKey": "AIzaSyD4S00DSgPGv3_CQTTn9etZAyOo-1pnxCQ",
+  "authDomain": "studio-6924184329-396b7.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "951981757096"
+};
