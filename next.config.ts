cca08d2dced5569275ce3d35d8b70d4c8ce4474a
Mohit Kochar <grizzlybearr12@gmail.com>
2026-01-06 11:29:45 +0000

    solve web console isse with image

--- next.config.ts
+++ next.config.ts
@@ -24,6 +24,12 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
+        protocol: 'https' ,
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
         protocol: 'https',
         hostname: 'picsum.photos',
         port: '',
